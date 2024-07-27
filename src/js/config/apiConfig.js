1bf1ea167e57bc134b681cefa1854bc4e6c867bc
Vlad Bezsonov <vd115313@gmail.com>
2024-07-27 13:14:27 +0200

    feat: get.data(cities, countries) from server

--- src/js/config/apiConfig.js
+++ src/js/config/apiConfig.js
@@ -1,5 +1,5 @@
 const config = {
-    url: 'https://openflights.org/'
+    url: 'https://aviasales-api.herokuapp.com'
 };
 
 export default config;
--- src/js/services/apiService.js
+++ src/js/services/apiService.js
@@ -1,11 +1,35 @@
 import axios from 'axios';
 import config from '../config/apiConfig';
 
+/**
+ * /Countries - array of countries
+ * /Cities - array of cities
+ * /Prices/Cheap - array available trips
+ */
 
 class Api {
     constructor(config) {
         this.url = config.url;
     }
+    async countries() {
+        try {
+            const response = await axios.get(`${this.url}/countries`)
+            return response.data
+        } catch(err) {
+            console.log(err);
+            return Promise.reject(err);
+        }
+    }
+    async cities() {
+        try {
+            const response = await axios.get(`${this.url}/cities`)
+            return response.data
+        } catch(err) {
+            console.log(err);
+            return Promise.reject(err);
+        }
+    }
+     async prices(params) {}
 }
 
 const api = new Api(config);
